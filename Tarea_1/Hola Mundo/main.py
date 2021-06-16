e73614cbcf87d5c15c63fdebec5580ec4ab537f0
urielmajb <urielmajb@gmail.com>
2021-06-16 13:51:52 -0600

    Python Curso 2

---
+++ Tarea_1/Hola Mundo/main.py
@@ -0,0 +1,52 @@
+# print ("Hola mundo con python woooo!")
+
+miVariable = "Hola mundo con python woooo!"
+print(miVariable)
+
+x = 10
+y = 2
+suma = x + y
+
+print(suma, x, y)
+
+print(id(x))
+print(id(y))
+print(id(suma))
+print(type(x))
+
+x = 15
+print(x)
+print(type(x))
+
+y = 3.14
+print(y)
+print(type(y))
+
+nombre = 'Urielma Jiron'
+print(nombre)
+print(type(nombre))
+
+# Cadena (String)
+miArtistafav = "Lady Gaga"
+print("Mi cantante favorita es: " + miArtistafav)
+
+Condicion = 1 > 3
+print(Condicion)
+
+#Funcion input, procesa la entrada del usuario.
+
+#num1 = int(input("Ingrese el primer numero"))
+#num2 = int(input("Ingrese el segundo numero"))
+#suma = num1 + num2
+#resta = num1 - num2
+#division = num1 / num2
+#print("Los resultados son: ", suma , " " , resta , " " , division)
+
+#Ejercicio Califiacion del dia
+
+#dia = input("¿Como estuvo el día (1 a 10)?")
+#print("Mi dia estuvo: ", dia)
+
+titulo = input("Proporciona el título: ")
+autor = input("Proporciona el autor: ")
+print(titulo, "fue escrito por " ,autor )
---
+++ Tarea_2/Operadores/main.py
@@ -0,0 +1,33 @@
+#Operadores
+
+#alto = int(input("Proporcionar el alto: "))
+#ancho = int(input("Proporcionar el ancho: "))
+#area = alto * ancho
+#perimetro = ((alto + ancho) * 2)
+#print(f'El area es: {area}' + f'y el perimetro: {perimetro}')
+
+#Incremento con reasignacion
+#miVariable = 11
+#print(miVariable)
+#miVariable -= 2
+#print(miVariable)
+#miVariable += 3
+#print(miVariable)
+
+#Operaciones de comparacion
+#a = int(input('Ingrese el 1er numero'))
+#b= input('Ingrese el 2d0 numero')
+
+#if a % 2 == 0:
+#print('El valor de {a} es numero par')
+#else:
+#print('El valor de {a} es impar')
+
+edad=18
+edadPersona= int(input('Escribe tu edad: '))
+
+if edadPersona >= edad:
+    print('Eres mayor de edad')
+else:
+   print('Eres menor de edad')
+
