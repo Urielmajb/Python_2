3fab5602e13e60799bd2f0e7d77df26e64c207ff
urielmajb <urielmajb@gmail.com>
2021-06-16 14:33:54 -0600

    operadores logicos tarea

--- Tarea_2/Operadores/main.py
+++ Tarea_2/Operadores/main.py
@@ -1,46 +1,52 @@
-#Operadores
-#alto = int(input("Proporcionar el alto: "))
-#ancho = int(input("Proporcionar el ancho: "))
-#area = alto * ancho
-#perimetro = ((alto + ancho) * 2)
-#print(f'El area es: {area}' + f'y el perimetro: {perimetro}')
+# Operadores
+# alto = int(input("Proporcionar el alto: "))
+# ancho = int(input("Proporcionar el ancho: "))
+# area = alto * ancho
+# perimetro = ((alto + ancho) * 2)
+# print(f'El area es: {area}' + f'y el perimetro: {perimetro}')
 
-#Incremento con reasignacion
-#miVariable = 11
-#print(miVariable)
-#miVariable -= 2
-#print(miVariable)
-#miVariable += 3
-#print(miVariable)
+# Incremento con reasignacion
+# miVariable = 11
+# print(miVariable)
+# miVariable -= 2
+# print(miVariable)
+# miVariable += 3
+# print(miVariable)
 
-#Operaciones de comparacion
-#a = int(input('Ingrese el 1er numero'))
-#b= input('Ingrese el 2d0 numero')
+# Operaciones de comparacion
+# a = int(input('Ingrese el 1er numero'))
+# b= input('Ingrese el 2d0 numero')
 
-#if a % 2 == 0:
-#print('El valor de {a} es numero par')
-#else:
-#print('El valor de {a} es impar')
-#edad=18
-#edadPersona= int(input('Escribe tu edad: '))
-#if edadPersona >= edad:
- #   print('Eres mayor de edad')
-#else:
- #  print('Eres menor de edad')
+# if a % 2 == 0:
+# print('El valor de {a} es numero par')
+# else:
+# print('El valor de {a} es impar')
+# edad=18
+# edadPersona= int(input('Escribe tu edad: '))
+# if edadPersona >= edad:
+#   print('Eres mayor de edad')
+# else:
+#  print('Eres menor de edad')
 
-#Operadores logicos
-#a= True
-#b= True
-#resultado = a and b
-#print(resultado)
-#valor = int(input('Escribe el valor: '))
-#valorMinimo = 0
-#valorMaximo = 5
-#dentroRango = valor >= valorMinimo and valor <= valorMaximo
-#if dentroRango:
-#print(f'El valor {valor} esta dentro de rango')
-#else:
-#print(f'El valor {valor} esta fuera de rango')
+# Operadores logicos
+# a= True
+# b= True
+# resultado = a and b
+# print(resultado)
+# valor = int(input('Escribe el valor: '))
+# valorMinimo = 0
+# valorMaximo = 5
+# dentroRango = valor >= valorMinimo and valor <= valorMaximo
+# if dentroRango:
+# print(f'El valor {valor} esta dentro de rango')
+# else:
+# print(f'El valor {valor} esta fuera de rango')
 
-vacaciones = True
-diaDescanso = True
+vacaciones = False
+diaDescanso = False
+tieneVacaciones = vacaciones or diaDescanso
+
+if tieneVacaciones:
+    print('Puede asistir al juego')
+else:
+    print('No puede asistir al juego')
