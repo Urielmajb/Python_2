e194288636a8933e7a74ca26b189a9878b142c46
urielmajb <urielmajb@gmail.com>
2021-06-16 14:20:06 -0600

    operadores logicos tarea

--- Tarea_2/Operadores/main.py
+++ Tarea_2/Operadores/main.py
@@ -43,4 +43,8 @@ valorMinimo = 0
 valorMaximo = 5
 
 dentroRango = valor >= valorMinimo and valor <= valorMaximo
-print (dentroRango)
+
+if dentroRango:
+    print(f'El valor {valor} esta dentro de rango')
+else:
+    print(f'El valor {valor} esta fuera de rango')
